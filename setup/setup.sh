204eb51b0fadd9de2cf2920579348989a3bd7805
robertsahlin <robert.sahlin@gmail.com>
2021-03-31 14:29:05 +0000

    comment import statements in setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,7 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 CLOUD_BUILD_SERVICE_ACCOUNT=${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com
 
-gcloud services enable cloudbuild.googleapis.com
+gcloud services enable cloudbuild.googleapis.com cloudresourcemanager.googleapis.com
 
 # Temporarily grant the cloud build service account the project admin role.
 gcloud projects add-iam-policy-binding $PROJECT_ID \
